5a33866756297dfe84eb123b9589414c321b6db4
Adriano Domingues <programadormovel@gmail.com>
2021-11-26 21:16:40 -0300

    ajustes na aula inicial de 26-11

--- src/main/java/com/example/demo/Aula2611.java
+++ src/main/java/com/example/demo/Aula2611.java
@@ -1,19 +1,36 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class Aula2611 {
 
     public static void teste(){
 
-        System.out.println("teste");
+        // Saída de dados
+        System.out.println("Açai com banana");
 
-        Pessoa p = new Pessoa();
-        p.nome = "Adão";
-        p.idade = 18;
+        // Criação de objeto
+        Pessoa p1 = new Pessoa();
+        
+        // Criação de objeto de entrada de dados
+        Scanner teclado = new Scanner(System.in);
+        
+        // Saída de dados - orientação ao usuário
+        System.out.println("Entre com uma idade: ");
+        
+        // Atribuição de dados via teclado - Entrada de Dados pelo teclado
+        p1.idade = teclado.nextInt();
 
-        System.out.println(p.nome);
-        System.out.println(p.idade);
+        // Saída de dados
+        System.out.println("A idade digitada foi " + p1.idade);
+        
+        // Chamada de Métodos
+        p1.falar("sozinho");
+        
+        System.out.println(p1.falar());
+        
 
     }
 
@@ -21,6 +38,16 @@ public class Aula2611 {
 }
 
 class Pessoa{
+    // Atributos
     String nome;
     int idade;
+    
+    // Métodos
+    public void falar(String parametro){
+        System.out.println("Falando ...." + parametro);
+    }
+    
+    public String falar(){
+        return "Falando com retorno...";
+    }
 }
